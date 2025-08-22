f9a9f53b3c6ac379aae496dff9cb89a2cda64456
KUROROSUKE <161828247+KUROROSUKE@users.noreply.github.com>
2025-08-22 23:31:23 +0900

    Add files via upload

--- Code/firebase-config.js
+++ Code/firebase-config.js
@@ -1,17 +1,18 @@
 // firebase-config.js
-// インライン禁止のCSPに対応するため、初期化を外部ファイルで行う。
+// インライン禁止CSPのため外部ファイルで初期化。
 // 自分のFirebaseプロジェクト設定に置き換えてください。
 
 (function initFirebase() {
-  // ここに自分の設定を貼る
+  // TODO: 自分の設定を貼る
   const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT_ID.firebaseio.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "SENDER_ID",
-    appId: "APP_ID"
+    apiKey: "AIzaSyBE8CK6ODzy0OrgPogLrE4IK9938rUF3ko",
+    authDomain: "homepoti-b61a7.firebaseapp.com",
+    databaseURL: "https://homepoti-b61a7-default-rtdb.firebaseio.com",
+    projectId: "homepoti-b61a7",
+    storageBucket: "homepoti-b61a7.firebasestorage.app",
+    messagingSenderId: "379862558289",
+    appId: "1:379862558289:web:a8f40e857d5ade3f35ba70",
+    measurementId: "G-W52MY9CN8L"
   };
   if (!firebase.apps.length) {
     firebase.initializeApp(firebaseConfig);
